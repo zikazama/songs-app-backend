645640da3d2005e8443eb32764972f1f01198fff
Fauzi <fauzi.fadhlurrohman@divusi.com>
2021-06-21 11:12:40 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,33 +1,50 @@
 /*  eslint linebreak-style: ["error", "windows"]  */
-require('dotenv').config();
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-const songs = require('./api/songs');
-const SongsService = require('./services/postgres/SongsService');
-const SongsValidator = require('./validator/songs');
+require("dotenv").config();
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+// songs
+const songs = require("./api/songs");
+const SongsService = require("./services/postgres/SongsService");
+const SongsValidator = require("./validator/songs");
+
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
 
   server.route(routes);
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   // eslint-disable-next-line no-console
